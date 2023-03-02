821dac3b180d48b4e06ea5e386fc4669234ba044
Dhanwan Prajapati <73684815+dhanwan@users.noreply.github.com>
2023-03-02 16:29:06 +0530

    Update nginx_reverseproxy.sh

--- nginx_reverseproxy.sh
+++ nginx_reverseproxy.sh
@@ -48,7 +48,7 @@ server {
         add_header X-Cache "HIT from Backend";
         add_header X-XSS-Protection "1; mode=block" always;
         add_header X-Content-Type-Options "nosniff" always;
-		add_header X-Frame-Options "SAMEORIGIN";
+	add_header X-Frame-Options "SAMEORIGIN";
     }
 
     location ~ /\.ht {
